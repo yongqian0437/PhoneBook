a44c452d26061c5994b894ba12ecc898b7532e78
Eddi3Ho <eddieho2001@gmail.com>
2021-05-10 17:21:21 +0800

    Merge branch 'main' of https://github.com/ijees/ijees into main

---
+++ application/models/rd_projects_model.php
@@ -0,0 +1,53 @@
+<?php
+
+class rd_projects_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    function insert($data)
+    {
+        $this->db->insert('rd_projects', $data);
+        if ($this->db->affected_rows() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function update($data, $id)
+    {
+        $this->db->where('rd_id ', $id);
+        if ($this->db->update('rd_projects', $data)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function delete($id)
+    {
+        $this->db->where('rd_id ', $id);
+        $this->db->delete('rd_projects');
+        if ($this->db->affected_rows() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function select_all()
+    {
+        return $this->db->get('rd_projects')->result();
+    }
+
+    function select_condition($condition)
+    {
+        $this->db->where($condition);
+        return $this->db->get('rd_projects')->result();
+    }
+}
--- application/models/universities_model.php
+++ application/models/universities_model.php
@@ -9,9 +9,9 @@ class universities_model extends CI_Model
         $this->load->database();
     }
 
-    function insert($data, $table_name)
+    function insert($data)
     {
-        $this->db->insert($table_name, $data);
+        $this->db->insert('universities', $data);
         if ($this->db->affected_rows() > 0) {
             return true;
         } else {
@@ -19,20 +19,20 @@ class universities_model extends CI_Model
         }
     }
 
-    function update($data, $id, $table_name)
+    function update($data, $id)
     {
         $this->db->where('uni_id', $id);
-        if ($this->db->update($table_name, $data)) {
+        if ($this->db->update('universities', $data)) {
             return true;
         } else {
             return false;
         }
     }
 
-    function delete($id, $table_name)
+    function delete($id)
     {
         $this->db->where('uni_id', $id);
-        $this->db->delete($table_name);
+        $this->db->delete('universities');
         if ($this->db->affected_rows() > 0) {
             return true;
         } else {
@@ -40,15 +40,14 @@ class universities_model extends CI_Model
         }
     }
 
-    function select_all($table_name)
+    function select_all()
     {
-        return $this->db->get($table_name)->result();
+        return $this->db->get('universities')->result();
     }
 
-    function select_condition($condition, $table_name)
+    function select_condition($condition)
     {
         $this->db->where($condition);
-        return $this->db->get($table_name)->result();
+        return $this->db->get('universities')->result();
     }
 }
-    
