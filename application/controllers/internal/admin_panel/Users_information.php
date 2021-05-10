376e968e4fb717ef942081d3b0f69661252178bf
YeePengTheong <82814849+YeePengTheong@users.noreply.github.com>
2021-05-10 17:49:06 +0800

    create users_information controller
    for admin's user information page

---
+++ application/controllers/internal/admin_panel/Users_information.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users_information extends CI_Controller 
+{
+    public function __construct()
+    {
+        parent:: __construct();
+        $this->load->model(['user_student_model','user_ep_model']);
+    }
+    
+    public function index()
+    {
+        
+      $data['title']= 'Student';
+      $data['users']=$this->db->get_where('users',['user_email'=>$this->session->userdata('user_email')])->row_array();
+      $data['user_student']=$this->db->get_where('user_student');
+        $this->load->view('templates/header',$data);
+        $this->load->view('templates/sidebar',$data);
+        $this->load->view('templates/topbar',$data);
+       
+        $this->load->model('student_model');
+        $result=$this->student_model->index();
+        $data=array('studentlist'=>$result);
+        $this->load->view('external/student',$data);
+        $this->load->view('templates/footer');
+        
+    }
+    public function detailstudent ($id)
+    {
+        $data['users']=$this->db->get_where('users',['user_email'=>$this->session->userdata('user_email')])->row_array();
+        $data['title']="Detail of Student";
+         $data['student']=$this->student_model->getdetail($id);        
+         $this->load->view('templates/header',$data);
+         $this->load->view('templates/sidebar',$data);
+         $this->load->view('templates/topbar',$data);
+         $this->load->view('external/stu_detail',$data);
+         $this->load->view('templates/footer'); 
+        
+    }
+
+}
+
+
+?>
