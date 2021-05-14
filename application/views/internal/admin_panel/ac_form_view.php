4d45f107c11de9334fa4cd5268a0ee2eac3096f4
YeePengTheong <82814849+YeePengTheong@users.noreply.github.com>
2021-05-15 01:26:38 +0800

    change the view of student and ac in the admin dashboard

--- application/views/internal/admin_panel/ac_form_view.php
+++ application/views/internal/admin_panel/ac_form_view.php
@@ -1,23 +1,83 @@
-<?php $title?> 
 <div class="container">
-    <div class="row mt-3">
-        <div class="col-md-6">
-        <div class="card">
-  <div class="card-header">
-    <b>Detail of Education Partner</b>
-  </div>
-  <div class="card-body">
-    <p class="card-title">Education Partner ID: <?=$ac['ac_id'];?></p>
-    <p class="card-title">Phone Number: <?=$ac['ac_phonenumber'];?>
-    <p class="card-text">Business email:<?=$ac['ac_businessemail'];?></p> 
-    <p class="card-text">Gender:<?=$ac['ac_university'];?></p> 
-    <p class="card-text">Nationality:<?=$ac['ac_nationality'];?></p>
-    <p class="card-text">Gender:<?=$ac['ac_gender'];?></p> 
-    <p class="card-text">Date of Birth:<?=$ac ['ac_dob'];?></p> 
-    <p class="card-text">Document:<?=$ac ['ac_document'];?></p>  
-    <a href="<?=base_url();?>internal/admin_panel/Admin_dashboard/users_accounts_nav" class="btn btn-primary">Back</a>
-  </div>
-</div>
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-7">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                           
+                            <div class="col-lg">
+                                <div class="p-5">                   
+                                <center>
+                                            <div class="pt-5 px-5" style="font-size:23px; letter-spacing: 8px; color:#787878; font-weight:700;">ACADEMIC COUNSELOR REGISTRATION PAGE</div>
+                                          </center>
+                                          <!-- Input fields (Form) -->
+                                          <form method="post" action="<?= base_url('user/login/Auth/ac_reg');?>">
+                                            <!-- Phone number and business email-->
+                                            <div class="form-row pt-4 px-3">
+                                              <div class="form-group col-md-7 px-2">
+                                                <input  class="form-control border-bottom" style="border: 0;" placeholder= "Phone Number: <?=$ac['ac_phonenumber'];?>" >
+                                              </div>
+                                              <div class="form-group col-md-7 px-2">
+                                              <input  class="form-control border-bottom" style="border: 0;" placeholder= "Business Email: <?=$ac['ac_businessemail'];?>" >
+                                              </div>
+                                            </div>
+                                            <!-- Nationality -->
+                                            <div class="form-row pt-3 px-3">
+                                              <div class="form-group col-md-12 px-2">
+                                              <input  class="form-control border-bottom" style="border: 0;" placeholder= "Nationality: <?=$ac['ac_nationality'];?>" >
+                                              </div>
+                                            </div>
+                                            <!-- Date and gender -->
+                                            <div class="form-row pt-3 pb-3 px-3">
+                                              <div class="form-group col-md-6 px-2">
+                                              <input  class="form-control border-bottom" style="border: 0;" placeholder= "Nationality: <?=$ac['ac_dob'];?>" >
+                                              </div>
+                                              <div class="form-holder mb-3 ml-3" style="align-self: flex-end; transform: translateY(4px);">
+                                                <div class="checkbox-tick">
+                                                  <label class="male ml-3">
+                                                  <?php if($ac ['ac_gender']=="Male"||"male"){?>
+                                                    <input type="radio" name="ac_gender" value="male" checked> Male<br>
+                                                    <span class="checkmark"></span>
+                                                  </label>
+                                                  <label class="female ml-5">
+                                                  <?php } else {?> 
+                                                    <input type="radio" name="ac_gender" value="female" checked> Female<br>
+                                                    <span class="checkmark"></span>
+                                                    <?php }?>
+                                                  </label>
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <!-- University  -->
+                                            <div class="form-row px-3">
+                                              <div class="form-group col-md-12 px-2">
+                                              <input  class="form-control border-bottom" style="border: 0;" placeholder= "Nationality: <?=$ac['ac_university'];?>" >
+                                              </div>
+                                            </div>
+                                            <!-- Upload Document -->
+                                            <div class="form-row pt-2 px-4">
+                                              <div class="form-group col-md-12 px-2">
+                                                <input type="file" class="custom-file-input" id="form-group" name="ac_document">
+                                                <label class="custom-file-label" for="customFile">Upload a file</label>
+                                              </div>
+                                            </div>
+                                          </form>    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
+
+    
     </div>
-</div>
+
+
+
--- application/views/internal/admin_panel/student_form_view.php
+++ application/views/internal/admin_panel/student_form_view.php
@@ -1,22 +1,79 @@
-<?php $title?> 
 <div class="container">
-    <div class="row mt-3">
-        <div class="col-md-6">
-        <div class="card">
-  <div class="card-header">
-    <b>Detail of Student</b>
-  </div>
-  <div class="card-body">
-    <p class="card-title">Student ID: <?=$student ['student_id'];?></p>
-    <p class="card-title">Phone Number: <?=$student ['student_phonenumber'];?></p>
-    <p class="card-text">Nationality:<?=$student ['student_nationality'];?></p> 
-    <p class="card-text">Gender:<?=$student ['student_gender'];?></p> 
-    <p class="card-text">Date of Birth:<?=$student ['student_dob'];?></p> 
-    <p class="card-text">Interest:<?=$student ['student_interest'];?></p> 
-    <p class="card-text">Current Level:<?=$student ['student_currentlevel'];?></p> 
-    <a href="<?=base_url();?>internal/admin_panel/Admin_dashboard/users_accounts_nav" class="btn btn-primary">Back</a>
-  </div>
-</div>
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-7">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                           
+                            <div class="col-lg">
+                                <div class="p-5">                   
+                                        <center>
+                                          <div class="pt-5 px-5" style="font-size:23px; letter-spacing: 8px; color:#787878; font-weight:700;">STUDENT REGISTRATION PAGE</div>
+                                        </center>
+                                        <!-- Input fields (Form) -->
+                                      <form>
+                                        <!-- Phone number and nationality-->
+                                      <center><b><p class="card-title">Student ID: <?=$student ['student_id'];?></p></b></center>
+                                        <div class="form-row pt-4 px-3">
+                                        <div class="form-group col-md-7 px-2">
+                                          <input  class="form-control border-bottom" style="border: 0;" placeholder= "Phone Number: <?=$student ['student_phonenumber'];?>" >
+                                        </div>
+                                        <div class="form-group col-md-5 px-2">
+                                          <input  class="form-control border-bottom" style="border: 0;" placeholder= "Nationality: <?=$student ['student_nationality'];?>" >
+                                        </div>
+                                        </div>
+                                        <!-- Date and gender -->
+                                        <div class="form-row pt-3 pb-3 px-3">
+                                          <div class="form-group col-md-6 px-2">
+                                            <input  class="form-control border-bottom" style="border: 0;" placeholder= "Date of Birth:<?=$student ['student_dob'];?>" >
+                                          </div>
+                                        <div class="form-holder mb-3 ml-3" style="align-self: flex-end; transform: translateY(4px);">
+                                            <div class="checkbox-tick">
+                                              <label class="male ml-3">
+                                              <?php if($student ['student_gender']=="Male"||"male"){?>
+                                                <input type="radio" name="student_gender" value="male" checked>Male<br>
+                                                <span class="checkmark"></span>
+                                              </label>
+                                              <label class="female ml-5">
+                                              <?php } else {?> 
+                                                <input type="radio" name="student_gender" value="female" checked> Female<br>
+                                                <span class="checkmark"></span>
+                                                <?php }?>
+                                              </label>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <!-- Interest -->
+                                        <div class="form-row px-3">
+                                          <div class="form-group col-md-12 px-2">
+                                          <input  class="form-control border-bottom" style="border: 0;" placeholder= "Interest:<?=$student ['student_interest'];?>" >
+                                          </div>
+                                        </div>
+                                        <!-- Current Level -->
+                                        <div class="form-row px-3">
+                                          <div class="form-group col-md-12 px-2">
+                                          <input  class="form-control border-bottom" style="border: 0;" placeholder= "Current Level:<?=$student ['student_currentlevel'];?>" >
+                                          <br>
+                                          <a href="<?=base_url();?>internal/admin_panel/Admin_dashboard/users_accounts_nav" class="btn btn-primary">Back</a>
+                                          </div>
+                                        </div>
+                                      </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
+
+    
     </div>
-</div>
+
+
+
