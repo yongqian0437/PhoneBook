170127b353aa39cabafc0ccbb0135fd6851a9bf1
jordanngkk <mjngkahkang@gmail.com>
2021-05-31 22:23:49 +0800

    course list and course detail page
    fixed typo in course detail page title, added new data to display in course list and course detail page

--- application/views/external/courses_detail_view.php
+++ application/views/external/courses_detail_view.php
@@ -1,17 +1,18 @@
 <link href="<?php echo base_url() ?>/assets/scss/courses-styles.scss" rel="stylesheet">
 
 <!-- Top Navigation -->
-<?php $this->load->view('external/templates/topnav');?>
+<?php $this->load->view('external/templates/topnav'); ?>
 
 <style>
-#logo{
-    border-radius:50% 50% 50% 50%;
-    width:20vh;
-    height:20vh;
-    object-fit: scale-down;
-    background-color:white;
-}
+    #logo {
+        border-radius: 50% 50% 50% 50%;
+        width: 20vh;
+        height: 20vh;
+        object-fit: scale-down;
+        background-color: white;
+    }
 </style>
+
 <body id="page-top">
 
     <!-- Page Wrapper -->
@@ -21,9 +22,9 @@
         <div id="content-wrapper" class="d-flex flex-column">
 
             <!-- Main Content -->
-            <div id="content" >
+            <div id="content">
                 <!-- Begin Page Content -->
-                <div class="container-fluid" >
+                <div class="container-fluid">
 
                     <!-- Content Row -->
                     <div class="d-flex container py-4">
@@ -36,7 +37,7 @@
                                                 <div class="col-4">
                                                     <div class="pt-5 pl-2 pb-1">
                                                         <div style="width:25vh; height:25vh; border-radius:100%; margin:auto; background-color:white;">
-                                                            <center><img  src="<?php echo base_url($uni_data->uni_logo); ?>" alt="uni_logo" id = "logo" class ="pt-5"></center>
+                                                            <center><img src="<?php echo base_url($uni_data->uni_logo); ?>" alt="uni_logo" id="logo" class="pt-5"></center>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -59,8 +60,8 @@
                                                 <div class="row">
                                                     <div>
                                                         <a type="button" class="btn btn-secondary course-ave-buttons">Apply</a>
-                                                        <a type="button" href = "<?php echo base_url() . 'external/Universities/university_detail/' . $courses->uni_id ?>" class="btn btn-secondary course-ave-buttons">View University</a>
-                                                        <a type="button" href = "<?php echo base_url() . 'user/chat/Chat/'?>" class="btn btn-secondary course-ave-buttons">Enquire</a>
+                                                        <a type="button" href="<?php echo base_url() . 'external/Universities/university_detail/' . $courses->uni_id ?>" class="btn btn-secondary course-ave-buttons">View University</a>
+                                                        <a type="button" href="<?php echo base_url() . 'user/chat/Chat/' ?>" class="btn btn-secondary course-ave-buttons">Enquire</a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -72,41 +73,71 @@
                                                 <!-- Card Content - Collapse -->
                                                 <div class="collapse show" id="collapseCard1">
                                                     <div class="card-body">
-                                                        <?php echo $courses->course_shortprofile ?>
+                                                        <h6><b>Course Area</b></h6>
+                                                        <?php echo $courses->course_area ?>
                                                         <br>
+                                                        <br>
+
+                                                        <h6><b>About</b></h6>
+                                                        <div style=" word-break: break-word; text-align: justify;"> <?php echo $courses->course_shortprofile ?></div>
+
+                                                        <br>
+                                                        <br>
+                                                        <h6><b>Career Oppurtunities</b></h6>
                                                         <?php echo $courses->course_careeropportunities ?>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="card shadow mb-4">
-                                                <!-- Card Header - Accordion -->
-                                                <a href="#collapseCard2" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                                            <!-- <div class="card shadow mb-4"> -->
+                                            <!-- Card Header - Accordion -->
+                                            <!-- <a href="#collapseCard2" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
                                                     <h6 class="m-0 font-weight-bold card-h-title">Programme Structure</h6>
-                                                </a>
-                                                <!-- Card Content - Collapse -->
-                                                <div class="collapse show" id="collapseCard2">
+                                                </a> -->
+                                            <!-- Card Content - Collapse -->
+                                            <!-- <div class="collapse show" id="collapseCard2">
                                                     <div class="card-body">
                                                         <?php echo $courses->course_structure ?>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="card shadow mb-4">
-                                                <!-- Card Header - Accordion -->
-                                                <a href="#collapseCard3" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
-                                                    <h6 class="m-0 font-weight-bold card-h-title">Fees</h6>
-                                                </a>
-                                                <!-- Card Content - Collapse -->
-                                                <div class="collapse show" id="collapseCard3">
-                                                    <div class="card-body">
-                                                        <p> RM<?php echo $courses->course_fee ?> -
-                                                            <?php echo $courses->course_duration ?> year(s)
-                                                        </p>
+                                            </div> -->
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <div class="d-flex card shadow mb-4">
+                                                        <!-- Card Header - Accordion -->
+                                                        <a href="#collapseCard3" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                                                            <h6 class="m-0 font-weight-bold card-h-title">Fees</h6>
+                                                        </a>
+                                                        <!-- Card Content - Collapse -->
+                                                        <div class="collapse show" id="collapseCard3">
+                                                            <div class="card-body">
+                                                                <p><b>Course Fee:</b> RM<?php echo $courses->course_fee ?> -
+                                                                    <?php echo $courses->course_duration ?> year(s)
+                                                                <p><b>Course intake:</b> <?php echo $courses->course_intake ?></p>
+                                                                <p><b>Course level:</b> <?php echo $courses->course_level ?></p>
+                                                                <p><b>Course location:</b> <?php echo $courses->course_country ?></p>
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
-
-
+                                                <div class="col-6">
+                                                    <div class=" d-flex card shadow mb-4">
+                                                        <!-- Card Header - Accordion -->
+                                                        <a href="#collapseCard3" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                                                            <h6 class="m-0 font-weight-bold card-h-title">Requirement</h6>
+                                                        </a>
+                                                        <!-- Card Content - Collapse -->
+                                                        <div class="collapse show" id="collapseCard3">
+                                                            <div class="card-body">
+                                                                <p><?php echo $courses->course_requirements ?></p>
+                                                                </p>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
+
                                             <br>
 
                                         <?php } ?>
@@ -118,7 +149,7 @@
 
                     </div>
 
+                </div>
+                <!-- /.container-fluid -->
             </div>
-            <!-- /.container-fluid -->
-        </div>
-        <!-- End of Main Content -->
+            <!-- End of Main Content -->
