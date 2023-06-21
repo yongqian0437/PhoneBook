22c8efb7d59a1cc9be29fb5985f8ff6080a65612
yongqian0437 <108520339+yongqian0437@users.noreply.github.com>
2023-06-21 12:18:20 +0800

    re

--- application/views/report_view.php
+++ application/views/report_view.php
@@ -1,21 +1,22 @@
 <!-- Set base url to javascript variable-->
-<script type="text/javascript">
-    var base_url = "<?php echo base_url();?>";
-
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js">
+    var base_url = "<?php echo base_url(); ?>";
 </script>
 
 
 <!-- Styles-->
 <style>
+    html {
+        scroll-behavior: smooth;
+    }
 
-html {
-scroll-behavior: smooth;
-}
-
+    #report_button {
+        background-color: #1cc88a;
+    }
 </style>
 
 <!-- Top Navigation -->
-<?php $this->load->view('templates/topnav');?>
+<?php $this->load->view('templates/topnav'); ?>
 
 <body id="page-top">
 
@@ -29,30 +30,21 @@ scroll-behavior: smooth;
             <div id="content">
 
                 <!-- Begin Page Content -->
-                <div style = 'background-color:white;' class="container-fluid">
+                <div style='background-color:white;' class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-2 px-4">
-                        <h1 class="h3 mb-0 text-gray-800 pt-4 font-weight-bold">Report</h1>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <!-- Page Heading -->
+                            <div class="d-sm-flex align-items-center justify-content-between mb-2 px-4">
+                                <h1 class="h3 mb-0 text-gray-800 pt-4 font-weight-bold">Report</h1>
+                            </div>
+                            <div class="py-2 px-4" style="text-align: justify; font-weight:500;">Reports</div>
+                        </div>
+                        <div class="col-md-4 pt-5 pr-5">
+                            <a id="report_button" class="btn btn-primary" style="float:right; width:auto;">Print Report</a>
+                        </div>
                     </div>
 
-              
-
-                </div>
-                <!-- /.container-fluid --> 
-                <div class = "row">
-                <div class="breadcrumb-wrapper col-xl-8">
-                            <ol class="breadcrumb" style="background-color:rgba(0, 0, 0, 0);">
-                                <li class="breadcrumb-item">
-                                    <a href="<?= base_url('reading_corner'); ?>"><i class="fas fa-tachometer-alt pr-2"></i>Reading Corner</a>
-                                </li>
-                                <li class="breadcrumb-item">
-                                    <a href="<?= base_url('quiz'); ?>">Quiz Report</a>
-                                </li>
-
-                            </ol>
-                        </div>
-                </div>
-
-            </div>
-            <!-- End of Main Content -->
+                    <div class="px-4 pb-4">
+                        <hr style=" width :100%; height:2px; background-color:#EAF4F4">
+                    </div>
